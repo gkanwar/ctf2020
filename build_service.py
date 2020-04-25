d8d1634d54741b4788237f53165b928d3bdef5d3
Gurtej Kanwar <gurtej@mit.edu>
2020-04-25 18:40:08 -0400

    Splits monitor implementation into internal file which is not provided

---
+++ build_service.py
@@ -0,0 +1,14 @@
+import glob
+import os
+import py_compile
+import shutil
+
+shutil.rmtree('./service_prod', ignore_errors=True)
+os.mkdir('./service_prod')
+for f in glob.glob('./service/*.py'):
+    shutil.copy(f, './service_prod')
+shutil.copytree('./service/static', './service_prod/static')
+# We "lost" the source for monitor_internal, only have .pyc
+py_compile.compile('./service_prod/monitor_internal.py', cfile='./service_prod/monitor_internal.pyc')
+os.remove('./service_prod/monitor_internal.py')
+
---
+++ service/monitor_internal.py
@@ -0,0 +1,34 @@
+### INTERNAL NOTE: This file is "lost", so user only gets the .pyc
+
+import json
+import socketserver
+import urllib.request
+from http.client import HTTPConnection
+# Do a little hacking
+HTTPConnection._encode_request = lambda self, r: r.encode('utf-8')
+
+class MonitorHandler(socketserver.StreamRequestHandler):
+    WEB_PORT = None
+    def handle(self):
+        r = urllib.request.urlopen(f'http://localhost:{MonitorHandler.WEB_PORT}/🙃', timeout=1)
+        if r.status != 200:
+            self.wfile.write(b'Internal error!\n')
+            return
+        body = r.read().decode('utf-8')
+        lines = body.split('\n')
+        if len(lines) < 2:
+            self.wfile.write(b'Internal error!\n')
+            return
+        env = json.loads(lines[1])
+        for key in env:
+            if key.startswith('SID'):
+                self.wfile.write((key[3:] + '\n').encode('utf-8'))
+
+class NonblockingServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    allow_reuse_address = True
+    daemon_threads = True
+
+def run_monitor(host, port, web_port):
+    MonitorHandler.WEB_PORT = web_port
+    with NonblockingServer((host, port), MonitorHandler) as server:
+        server.serve_forever()
