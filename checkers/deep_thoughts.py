1fb98791864a888191bec6258c748e0aa2d74112
Gurtej Kanwar <gurtej@mit.edu>
2020-05-02 18:28:29 +0000

    Implements monitor service checker

--- checkers/deep_thoughts.py
+++ checkers/deep_thoughts.py
@@ -2,7 +2,10 @@ from ctf_gameserver.checker import BaseChecker, OK, TIMEOUT, NOTWORKING, NOTFOUN
 import pickle
 import random
 import requests
+import socket
 import string
+
+MONITOR_PORT = 7778
 
 class StatusChecker(BaseChecker):
     def __init__(self, tick, team, service, ip):
@@ -28,18 +31,29 @@ class StatusChecker(BaseChecker):
         return self.req(s, 'post', path, data=data, timeout=timeout)
     def get(self, s, path, *, timeout=10):
         return self.req(s, 'get', path, data=None, timeout=timeout)
-    
-    def place_flag(self):
+
+    def get_or_create_user(self, tick, *, create=True):
+        ident = f'creds_status_{self._service}_{self._team}_{tick}'
+        blob = self.retrieve_blob(ident)
+        if blob is not None:
+            return OK, pickle.loads(blob)
+        self.logger.info(f'Did not find user for tick {tick} with ident {ident}, creating...')
+        if not create:
+            raise RuntimeError(f'User blob should already exist with ident {ident}')
         username = 'test' + ''.join(random.choices(string.digits , k=16))
         password = ''.join(random.choices(string.printable, k=32))
         creds = {'username': username, 'password': password}
-        blob = pickle.dumps(creds)
-        ident = f'creds_status_{self._service}_{self._team}_{self.tick}'
-        print('saving ident', ident)
-        self.store_blob(ident, blob)
         s = requests.Session()
         status, _ = self.post(s, '/register', data=creds)
+        if status != OK: return status, None
+        blob = pickle.dumps(creds)
+        self.store_blob(ident, blob)
+        return OK, creds
+
+    def place_flag(self):
+        status, creds = self.get_or_create_user(self.tick)
         if status != OK: return status
+        s = requests.Session()
         handshake = random.choices(string.ascii_lowercase + string.digits, k=10)
         status, _ = self.post(s, '/login', data={**creds, 'handshake': handshake})
         if status != OK: return status
@@ -51,9 +65,8 @@ class StatusChecker(BaseChecker):
         return OK
 
     def check_flag(self, tick):
-        ident = f'creds_status_{self._service}_{self._team}_{tick}'
-        print('loading ident', ident)
-        creds = pickle.loads(self.retrieve_blob(ident))
+        status, creds = self.get_or_create_user(tick, create=False)
+        if status != OK: return status
         s = requests.Session()
         status, _ = self.post(s, '/login', data=creds)
         if status != OK: return status
@@ -66,4 +79,29 @@ class StatusChecker(BaseChecker):
         return OK
 
     def check_service(self):
-        return OK
+        status, creds = self.get_or_create_user(self.tick)
+        if status != OK: return status
+        s = requests.Session()
+        status, _ = self.post(s, '/login', data=creds) # read-only (no handshake)
+        if status != OK: return status
+        cookies = s.cookies.get_dict()
+        if not 'session_id' in cookies:
+            self.logger.warn(f'No session_id in cookies')
+            return NOTWORKING
+        if not cookies['session_id'].startswith('SID'):
+            self.logger.warn(f'Cookie does not start with SID {cookies["session_id"]}')
+            return NOTWORKING
+        sess_id_prefix = cookies['session_id'][3:3+6]
+        self.logger.info(f'Looking for my sess id prefix {sess_id_prefix}')
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self._ip, MONITOR_PORT))
+        data = b''
+        while True:
+            chunk = s.recv(4096)
+            if chunk == b'': break
+            data += chunk
+        data = data.decode('utf-8')
+        lines = data.split('\n')
+        for line in lines:
+            if sess_id_prefix in line: return OK
+        return NOTWORKING
