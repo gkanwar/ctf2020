81d6fa0d1df3fc1ddeefb46310793972d13d6621
Gurtej Kanwar <gurtej@mit.edu>
2020-05-03 02:29:40 +0000

    Adds bundle passwords

--- infra/03_make_team_bundles.sh
+++ infra/03_make_team_bundles.sh
@@ -8,13 +8,18 @@ if [[ "$NUM_MEMBERS" == "" ]]; then
     echo "Must set NUM_MEMBERS"
     exit 1
 fi
+if [[ "$BUNDLE_PASSWORD" == "" ]]; then
+    echo "Must set BUNDLE_PASSWORD"
+    exit 1
+fi
 
 cd team_confs
 for t in $(seq 1 ${NUM_TEAMS}); do
-    (
+    zip -P ${BUNDLE_PASSWORD} -r team${t}.zip . -i $(
 	echo "team${t}_password.txt"
 	for i in $(seq 1 ${NUM_MEMBERS}); do
 	    echo "team${t}_${i}.conf"
 	done
-    ) | tar cvzf team${t}.tgz -T -
+        )
+    # | tar cvzf team${t}.tgz -T -
 done
